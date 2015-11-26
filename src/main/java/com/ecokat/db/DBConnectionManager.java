a6850fd85ac31da60c5b2f568b044552df0a9130
Peyloride <okanb3@gmail.com>
2015-11-26 13:32:31 +0200

    Dependencyler eklendi, primefaces ve mysql connector java

--- src/main/java/com/ecokat/db/DBConnectionManager.java
+++ src/main/java/com/ecokat/db/DBConnectionManager.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package tr.gov.eba.projeyonetim.db;
+package com.ecokat.db;
 
 import java.sql.*;
 import java.util.ResourceBundle;
